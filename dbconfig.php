ed6b4295678763e3473c4ac6e6c1abf7f94d0ab7
skt47 <fusing0000@gmail.com>
2023-10-16 10:59:39 +0800

    deleted password from git

--- dbconfig.php
+++ dbconfig.php
@@ -3,9 +3,9 @@
 連線資料庫用的副程式
 */
 $host = 'localhost'; //執行DB Server 的主機
-$user = ''; //登入DB用的DB 帳號
-$pass = ''; //登入DB用的DB 密碼
-$dbName = ''; //使用的資料庫名稱
+$user = 'shiki'; //登入DB用的DB 帳號
+$pass = 'password'; //登入DB用的DB 密碼
+$dbName = 'userdb'; //使用的資料庫名稱
 /* $db 即為未來執行SQL指令所使用的物件 */
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 
